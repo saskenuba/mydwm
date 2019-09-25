d85ce2d583bf2c0465cb676e276f3be6af93ea09
Martin <martin@hotmail.com.br>
2019-09-25 19:59:06 -0300

    now using alacritty term with dwm

--- config.h
+++ config.h
@@ -66,8 +66,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *rangercmd[]  = { "st", "ranger", NULL};
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *rangercmd[]  = { "alacritty", "-e", "ranger", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
